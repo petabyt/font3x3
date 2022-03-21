62d75a420e9e5fd0381b4264fda51fe4c9351939
Daniel C <brikbusters@gmail.com>
2022-03-21 10:40:24 -0400

    add demo, change lowercase to uppercase, change field name

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ This is a 3x3 font. It's about as small as you can get,
 and it isn't really even readable. But if you need a quick  
 font for a LED matrix, this should work.  
 
-It contains uppercase A-Z (access them as lowercase) and a few  
-symbols. It's written in the same format as https://github.com/petabyt/font/.  
+It contains uppercase A-Z and a few symbols. It's written  
+in the same format as https://github.com/petabyt/font/.  
 
 I designed this for use on the ScrollBit, but never got around to  
-using it for anything interesting. Feel free to use for whatever.  
+using it for anything interesting. Feel free to use it under the MIT.  
---
+++ demo.c
@@ -0,0 +1,99 @@
+// Unicode terminal printer
+// Try it out: `cc *.c; ./a.out`
+// (This program is modified from https://github.com/petabyt/font)
+
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <string.h>
+
+#include "font3x3.h"
+
+#define SCREEN_WIDTH 3 * 30
+#define SCREEN_HEIGHT 3 * 3 + 2
+
+char buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
+
+void drawPixel(int x, int y) {
+	buffer[y][x] = 1;
+}
+
+int printChar(int x, int y, char c) {
+	// Loop to "null terminator character"
+	int match = 0;
+	for (int l = 0; font[l].letter != 0; l++) {
+		if (font[l].letter == c) {
+			match = l;
+			break;
+		}
+	}
+
+	// Loop through 7 high 5 wide monochrome font
+	int maxLength = 0;
+	for (int py = 0; py < 3; py++) {
+		for (int px = 0; px < 3; px++) {
+			if (font[match].code[py][px] == '#') {
+				drawPixel(x + px, y + py);
+
+				// Dynamix width character spacing
+				if (px > maxLength) {
+					maxLength = px;
+				}
+			}
+		}
+	}
+
+	return maxLength;
+}
+
+int printString(int x, int y, char *string) {
+	int cx = x;
+	int cy = y;
+
+	for (int c = 0; string[c] != '\0'; c++) {
+		int length;
+		if (string[c] == ' ') {
+			length = 5;
+		} else {
+			length = printChar(cx, cy, string[c]) + 3;
+		}
+
+		cx += length;
+
+		// Text wrap after space
+		// Fine tune this to your liking.
+		if (cx > SCREEN_WIDTH - 20 && string[c] == ' ') {
+			cx = x;
+			cy += 8;
+		}
+	}
+
+	return cy;
+}
+
+int main() {
+	memset(buffer, 0, sizeof(buffer));
+
+	printString(1, 1, "HELLO WORLD");
+
+	setlocale(LC_CTYPE, "");
+
+	// Use upper half, lower half, and full to simulate graphics
+	for (int y = 0; y < SCREEN_HEIGHT - 1; y += 2) {
+		for (int x = 0; x < SCREEN_WIDTH; x++) {
+			if (buffer[y][x] && buffer[y + 1][x]) {
+				printf("%ls", L"\u2588");
+			} else if (buffer[y + 1][x]) {
+				printf("%ls", L"\u2584");
+			} else if (buffer[y][x]) {
+				printf("%ls", L"\u2580");
+			} else {
+				putchar(' ');
+			}
+		}
+
+		putchar('\r');
+	}
+
+	return 0;
+}
